19c47561d483f3fdf01b6c93be7f8d28d5c62e39
SixteenFtheDJ <SixteenFtheDJ@gmail.com>
2015-03-16 00:28:29 +0000

    Un peu de ménage

--- symb_tab/symb_tab.h
+++ symb_tab/symb_tab.h
@@ -18,23 +18,40 @@ typedef struct symbole
 
 } symbole;
 
+int compare_symb (list_node * node,void * valeur);
+
+void display_symb(list_node * node);
+
+void print_symb(symbole *symb);
 
 
-//initialiser la table
+
+
 void init_table();
+
+symbole *get_symbole(char *name);
+
 
 
 //retourne l'adresse pour le nom d'un element, renvoie -1 si l'élément n'existe pas
 int get_id_for_name(char * name);
 
-symbole * symb_pop();
 
-// mettre un element dans la table
+
+
+symbole* symb_pop();
+
+
 int insert(char * nom,int state);
 
-//changer le flag d'un élément
+
 int change_state(int newstate,char * name);
 
+int get_state(char *name);
+
+void print_tab_symb();
+
 void destroy_table();
+
 
 #endif
