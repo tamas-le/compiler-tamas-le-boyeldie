5f347b8657fe3b77648d191dc4dbdf52d107eef8
SixteenFtheDJ@gmail.com <SixteenFtheDJ@gmail.com@89dbe142-b41d-5a58-d87e-7ede2192c7dd>
2015-04-02 13:58:29 +0000

    Ajout de l'API pour gérer les instructions

---
+++ trunk/interpreter/list_instructions.h
@@ -0,0 +1,36 @@
+#include "../list/list.h"
+
+#ifndef INST
+#define INST
+
+typedef enum type_action{
+	ADD,MUL,SOU,DIV,COP,AFC,JMP,JMF,INF,SUP,EQU,PRI
+}type_action;
+
+typedef struct instruction
+{
+	int num_ligne;
+	type_action action;
+	int arg [3];
+
+} instruction;
+
+void display_instruction(list_node * node);
+
+void init_list_instruction();
+
+void add_instruction(int num_ligne,type_action action,int arg1,int arg2,int arg3);
+
+void go();
+
+void run_instruction(instruction *i);
+
+void destroy_list_inst();
+
+void print_list_instruction();
+
+
+
+#endif
+
+
