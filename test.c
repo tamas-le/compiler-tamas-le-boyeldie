fc360e84c39a53843f5619f630804adf99df17fc
SixteenFtheDJ <SixteenFtheDJ@gmail.com>
2015-04-03 07:02:24 +0000

    VERSION STABLE : Interpreter non optimisé mais fonctionnel / Compilater non optimisé mais fonctionnel

--- test.c
+++ test.c
@@ -1,12 +1,16 @@
 
 int main() {
 	int i=0;
-	int j=222;
-	while(i<5){
-		printf(i);
+	int j=2;
+	int k=1;
+	while(i<10){
+		if ((i/2)==0){
+			printf(j);
+		} else {
+			printf(k);
+		}
+
 		i=i+1;
 	}
-
-	printf(j);
 
 }
